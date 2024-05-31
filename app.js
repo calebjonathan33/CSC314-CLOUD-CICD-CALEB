e31e9fcc8e884afdec35858bee567bd1014b04f9
calebjonathan33 <calebjonathan33@gmail.com>
2024-05-31 12:09:25 +0100

    version

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 8080;
-app.use(express.static("public"));
+const port = 8080;
+app.use(express.static('public'));
 
-// app.get('/', (req, res) => res.send('Hello World!'));
+//app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port);
 console.log(`App runnning on http://localhost:${port}`);
